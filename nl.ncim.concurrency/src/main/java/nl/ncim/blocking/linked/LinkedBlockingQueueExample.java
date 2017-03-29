c2694b694ada1a3df98eecccfe896fad10da90ea
rvanrijn <r.van.rijn@ncim.nl>
2017-03-29 17:16:28 +0200

    Added Linkedblockingqueue example

---
+++ nl.ncim.concurrency/src/main/java/nl/ncim/blocking/linked/LinkedBlockingQueueExample.java
@@ -0,0 +1,23 @@
+package nl.ncim.blocking.linked;
+
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class LinkedBlockingQueueExample {
+
+    public static void main(String[] args) {
+        LinkedBlockingQueue queue = new LinkedBlockingQueue(10);
+
+        Producer producer = new Producer(queue);
+        ObservingConsumer obsConsumer = new ObservingConsumer(queue, producer);
+        RemovingConsumer remConsumer = new RemovingConsumer(queue, producer);
+
+        Thread producerThread = new Thread(producer);
+        Thread obsConsumerThread = new Thread(obsConsumer);
+        Thread remConsumerThread = new Thread(remConsumer);
+
+        producerThread.start();
+        obsConsumerThread.start();
+        remConsumerThread.start();
+    }
+}
