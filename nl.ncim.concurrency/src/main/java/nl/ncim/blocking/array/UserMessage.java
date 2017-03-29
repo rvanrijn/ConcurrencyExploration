3901ccbfc10bcca3d46187aac3731d23d61a9913
rvanrijn <r.van.rijn@ncim.nl>
2017-03-29 17:01:58 +0200

    Added missing UserMessage class

---
+++ nl.ncim.concurrency/src/main/java/nl/ncim/blocking/array/UserMessage.java
@@ -0,0 +1,14 @@
+package nl.ncim.blocking.array;
+
+public class UserMessage {
+
+    private String userMessage;
+
+    public UserMessage(String string) {
+        this.userMessage = string;
+    }
+
+    public String getMsg() {
+        return userMessage;
+    }
+}
